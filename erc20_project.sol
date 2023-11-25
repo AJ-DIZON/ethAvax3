5153b89733d098a4149b1813f7dcd33dde8dde7e
AJ-DIZON <61196811+AJ-DIZON@users.noreply.github.com>
2023-11-25 23:00:35 +0800

    Update erc20_project.sol

--- erc20_project.sol
+++ erc20_project.sol
@@ -15,7 +15,7 @@ contract Mtoken is ERC20 , Ownable{
         //100 tokens in constructer
     }
 
-                                    // Here only the contract owner can mint new tokens. Double check it please
+                                    // Here only the contract owner can mint new tokens. Please check it again it does work.
     function mintToken(uint amount) external onlyOwner {
         uint minted = amount * 10 ** uint(decimals());
         _mint(msg.sender, minted);
